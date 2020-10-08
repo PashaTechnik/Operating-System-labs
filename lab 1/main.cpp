e09f89bec519b057118b34e70d2f6760d0d9b483
Pasha Technik <pacha120431@gmail.com>
2020-10-08 15:17:38 +0300

    finish allocator

--- lab 1/main.cpp
+++ lab 1/main.cpp
@@ -1,172 +1,175 @@
 #include <iostream>
-
-#define HEAP_SIZE 1024
-//static char buffer[HEAP_SIZE];
+#include <zconf.h>
 
 
-void* alloc(size_t sz)
+using namespace std;
+
+
+
+
+struct header_t
 {
-    static size_t off = 0;
-    char* ptr = 0;
-    if(off + sz < HEAP_SIZE)
-    {
-        ptr = (char*)off;
-        off += sz;
-    }
+    size_t size;
+    unsigned is_free;
+    struct header_t *next;
+};
 
-    return ptr;
+
+struct header_t *head = NULL, *tail = NULL;
+
+
+
+struct header_t *get_free_block(size_t size)
+{
+    struct header_t *curr = head;
+    while(curr) {
+        if (curr->is_free && curr->size >= size)
+            return curr;
+        curr = curr->next;
+    }
+    return NULL;
 }
 
+
+void *malloc(size_t size)
+{
+    size_t total_size;
+    void *block;
+    struct header_t *header;
+
+    if (!size)
+        return NULL;
+
+
+    header = get_free_block(size);
+
+    if (header) {
+        header->is_free = 0;
+        return (void*)(header + 1);
+    }
+
+    total_size = sizeof(struct header_t) + size;
+    block = sbrk(total_size);
+    if (block == (void*) -1) {
+        return NULL;
+    }
+
+    header = static_cast<header_t *>(block);
+    header->size = size;
+    header->is_free = 0;
+    header->next = NULL;
+    if (!head)
+        head = header;
+    if (tail)
+        tail->next = header;
+    tail = header;
+
+    return (void*)(header + 1);
+}
+
+
+void free(void *block)
+{
+    struct header_t *header, *tmp;
+    void *programbreak;
+
+    if (!block)
+        return;
+
+
+
+    header = (struct header_t*)block - 1;
+
+
+    programbreak = sbrk(0);
+
+
+    if ((char*)block + header->size == programbreak) {
+
+        if (head == tail) {
+            head = tail = NULL;
+        }
+
+        else {
+            tmp = head;
+            while (tmp) {
+                if(tmp->next == tail) {
+                    tmp->next = NULL;
+                    tail = tmp;
+                }
+                tmp = tmp->next;
+            }
+        }
+
+        sbrk(0 - sizeof(struct header_t) - header->size);
+
+        return;
+    }
+    header->is_free = 1;
+
+}
+
+void *realloc(void *block, size_t size)
+{
+    struct header_t *header;
+    void *ret;
+
+
+    if (!block || !size)
+        return malloc(size);
+
+
+    header = (struct header_t*)block - 1;
+
+    if (header->size >= size)
+        return block;
+
+    ret = malloc(size);
+
+    if (ret) {
+        memcpy(ret, block, header->size);
+
+        free(block);
+    }
+
+    return ret;
+}
+
+void print_mem_list()
+{
+    header_t *curr = head;
+    printf("head = %p, tail = %p \n", (void*)head, (void*)tail);
+    while(curr) {
+        printf("addr = %p, size = %zu, is_free=%u, next=%p\n",
+               (void*)curr, curr->size, curr->is_free, (void*)curr->next);
+        curr = curr->next;
+    }
+}
 
 
 int main() {
 
 
 
-    std::cout<<alloc(1)<<std::endl;
-    std::cout<<alloc(1)<<std::endl;
-    std::cout<<alloc(1)<<std::endl;
-    std::cout<<alloc(5)<<std::endl;
-    std::cout<<alloc(1000)<<std::endl;
-    std::cout<<alloc(5)<<std::endl;
-    std::cout<<alloc(5)<<std::endl;
-    std::cout<<alloc(5)<<std::endl;
-    std::cout<<alloc(1)<<std::endl;
-    std::cout<<alloc(1)<<std::endl;
-    std::cout<<alloc(200)<<std::endl;
-    std::cout<<alloc(12)<<std::endl;
+    cout<<malloc(1)<<endl;
+    cout<<malloc(1)<<endl;
+    cout<<malloc(1)<<endl;
+
+    print_mem_list();
+    free(head->next);
+    print_mem_list();
+    malloc(2);
+    print_mem_list();
+
+
+    print_mem_list();
+
+    malloc(1);
+
+    print_mem_list();
+
+
 
 
     return 0;
 }
-
-
-
-
-//class Memory
-//{
-//public:
-//    Memory();
-//    int getCM();
-//    char* mem;
-//    void* mem_alloc(int);
-//    void mem_free(void*);
-//    void* mem_realloc(void*, int);
-//private:
-//    int cm = 0;
-//};
-//
-//Memory::Memory()
-//{
-//    mem = new char[1024];
-//    cm++;
-//
-//    int* s = (int*)mem;
-//    *s=10240;
-//}
-//
-//int Memory::getCM() {
-//    return cm;
-//}
-//
-//void* Memory::mem_alloc(int size) {
-//
-//
-//
-//    int p = 0;
-//    int rozp = 0;
-//    int pp;
-//    bool fl = false;
-//    for (int i = 0; i < cm; i++) {
-//        rozp += p;
-//        pp = *(int*)(mem+rozp);
-//        p = pp/10;
-//        if ((size <= p) && (pp % 2 == 0)) {
-//            fl = true;
-//            break;
-//        }
-//    }
-//    if (fl) {
-//        int*re = (int*)(mem+rozp);
-//        *re = size * 10 + 1;
-//        if (p - size != 0) {
-//            cm++;
-//            int*ree = (int*)(mem+rozp+size);
-//            *ree = (p - size)*10;
-//        }
-//
-//        return mem+rozp;
-//    } else {
-//        return NULL;
-//    }
-//}
-//
-//void Memory::mem_free(void* addr) {
-//
-//    clock_t start=clock();
-//    if ((*((int*)(addr)))%2 == 1) {
-//        (*((int*)(addr)))--;
-//    }
-//
-//    if ((char*)addr < mem+1024) {
-//        if ((*(int*)((char*)(addr)+((*((int*)(addr)))/10))) % 2 == 0) {
-//            (*((int*)(addr))) = (((*((int*)(addr)))/10) + (*(int*)((char*)(addr)+((*((int*)(addr)))/10)))/10)*10;
-//            cm--;
-//        }
-//    }
-//
-//    if ((char*)addr != mem) {
-//        char* c = mem;
-//        int pam = 0;
-//        while ((char*)addr != (c+((*((int*)c))/10))){
-//
-//            int pam = (*((int*)c))/10;
-//            c += pam;
-//        }
-//        c -= pam;
-//        if (((*((int*)c)) % 2 == 0) && (c >= mem)) {
-//            (*((int*)(c))) = (((*((int*)(c)))/10) + (*(int*)((c)+((*((int*)(c)))/10)))/10)*10;
-//            cm--;
-//        }
-//    }
-//    clock_t end=clock();
-//    std::cout << "START: " << start << " END: " << end << " TIME: " << end-start << std::endl;
-//}
-//
-//void* Memory::mem_realloc(void* addr, int size) {
-//
-//    int* bup = new int[cm];
-//    int cmm = cm;
-//    int p = 0;
-//    for(int i = 0; i < cm; i++) {
-//        bup[i] = *((int*)(mem+p));
-//        p += (bup[i]/10);
-//    }
-//
-//    int siz = (*(int*)addr)/10;
-//    if (size < siz) {
-//        siz = size;
-//    }
-//    mem_free(addr);
-//    void* c = mem_alloc(size);
-//
-//    if (c != NULL) {
-//
-//        for (int i = 4; i < siz; i++) {
-//            *((char*)(c)+i) = *((char*)(addr)+i);
-//        }
-//    } else {
-//
-//
-//        p = 0;
-//        cm = cmm;
-//        for(int i = 0; i < cm; i++) {
-//            *((int*)(mem+p)) = bup[i];
-//            p += (bup[i]/10);
-//        }
-//        return addr;
-//
-//    }
-//    return c;
-//
-//}
